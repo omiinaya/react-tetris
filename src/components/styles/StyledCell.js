244b62b56464f8ee79995ac4baffe9ef36668c8e
Omar Minaya <omiinaya@gmail.com>
2020-08-31 03:01:48 -0400

    dimming borders again for a better look.

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledCell = styled.div`
   width: auto;
   background: rgba(${props => props.color}, 0.8);
-  -webkit-box-shadow:inset 0px 0px 0px 0.5px 	#696969;
-  -moz-box-shadow:inset 0px 0px 0px 0.5px #696969;
-  box-shadow:inset 0px 0px 0px 0.5x #696969;
+  -webkit-box-shadow:inset 0px 0px 0px 0.5px rgba(105, 105, 105, 0.5);
+  -moz-box-shadow:inset 0px 0px 0px 0.5px rgba(105, 105, 105, 0.5);
+  box-shadow:inset 0px 0px 0px 0.5x rgba(105, 105, 105, 0.5);
 `;
